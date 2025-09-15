c72b0bced281d4ebe66ab39b2f0a73ba1fd2fb67
Whitemehat <wong1234.pe@gmail.com>
2025-09-15 13:58:19 +0700

    submit

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Peter"
+last_name = "Manot"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Peter"
+last_name = "Manot"
+whole_name = first_name + " " + last_name
+print(whole_name)
